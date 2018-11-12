c7ba5be6a92af3d2cbcac05d3118becbbe2768e8
Douglas Mathias <douglasmathias@gmail.com>
2018-11-12 08:49:38 +0000

    Adding missing tests

--- WaesDiff/WaesDiff.Tests/UnitTests/API/DiffApiServiceTest.cs
+++ WaesDiff/WaesDiff.Tests/UnitTests/API/DiffApiServiceTest.cs
@@ -3,8 +3,11 @@
     using Microsoft.Extensions.Options;
     using Moq;
     using System;
+    using System.Threading.Tasks;
+
     using WaesDiff.API.Services;
     using WaesDiff.Domain.Enum;
+    using WaesDiff.Domain.Models;
     using WaesDiff.Domain.Services;
     using WaesDiff.Domain.Settings;
     using WaesDiff.Infrastructure.Repository;
@@ -17,6 +20,7 @@
         public static void SaveDataWithEmptyDataReturnException()
         {
             //Arrange
+            const int id = 0;
             var settings = new Settings { Messages = new MessageSettings() };
             IOptions<Settings> options = Options.Create(settings);
             var mockDataRepository = new Mock<IDataRepository>();
@@ -25,13 +29,14 @@
             var diffApiService = new DiffApiService(options, mockDataRepository.Object, mockDiffService.Object);
 
             //Act & Assert
-            Assert.ThrowsAsync<ArgumentException>(() => diffApiService.SaveData(0, null, EnumDataType.Left)).ConfigureAwait(false);
+            Assert.ThrowsAsync<ArgumentException>(() => diffApiService.SaveData(id, null, EnumDataType.Left)).ConfigureAwait(false);
         }
 
         [Fact]
         public static void SaveDataWithInvalidBase64DataReturnException()
         {
             //Arrange
+            const int id = 0;
             var settings = new Settings { Messages = new MessageSettings() };
             IOptions<Settings> options = Options.Create(settings);
             var mockDataRepository = new Mock<IDataRepository>();
@@ -40,7 +45,58 @@
             var diffApiService = new DiffApiService(options, mockDataRepository.Object, mockDiffService.Object);
 
             //Act & Assert
-            Assert.ThrowsAsync<ArgumentException>(() => diffApiService.SaveData(0, "123", EnumDataType.Left)).ConfigureAwait(false);
+            Assert.ThrowsAsync<ArgumentException>(() => diffApiService.SaveData(id, "123", EnumDataType.Left)).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public static void GetDataWithInvalidIdReturnException()
+        {
+            //Arrange
+            const int id = -1;
+            var settings = new Settings { Messages = new MessageSettings() };
+            IOptions<Settings> options = Options.Create(settings);
+            var mockDataRepository = new Mock<IDataRepository>();
+            var mockDiffService = new Mock<IDiffService>();
+
+            var diffApiService = new DiffApiService(options, mockDataRepository.Object, mockDiffService.Object);
+
+            //Act & Assert
+            Assert.ThrowsAsync<ArgumentException>(() => diffApiService.GetDiff(id)).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public static void GetDataWithValidIdWithOneDataForDiffReturnException()
+        {
+            //Arrange
+            const int id = 200;
+            var settings = new Settings { Messages = new MessageSettings() };
+            IOptions<Settings> options = Options.Create(settings);
+            var mockDataRepository = new Mock<IDataRepository>();
+            var mockDiffService = new Mock<IDiffService>();
+
+            var diffApiService = new DiffApiService(options, mockDataRepository.Object, mockDiffService.Object);
+            diffApiService.SaveData(id, Util.DataEntitySameSizeA.Data, EnumDataType.Left).ConfigureAwait(false);
+
+            //Act & Assert
+            Assert.ThrowsAsync<ArgumentException>(() => diffApiService.GetDiff(id)).ConfigureAwait(false);
+        }
+
+        [Fact]
+        public static void GetDataWithValidIdForDiffReturnException()
+        {
+            //Arrange
+            const int id = 201;
+            var settings = new Settings { Messages = new MessageSettings() };
+            IOptions<Settings> options = Options.Create(settings);
+            var mockDataRepository = new Mock<IDataRepository>();
+            var mockDiffService = new Mock<IDiffService>();
+
+            var diffApiService = new DiffApiService(options, mockDataRepository.Object, mockDiffService.Object);
+            diffApiService.SaveData(id, Util.DataEntitySameSizeA.Data, EnumDataType.Left).ConfigureAwait(false);
+            diffApiService.SaveData(id, Util.DataEntitySameSizeA.Data, EnumDataType.Right).ConfigureAwait(false);
+
+            //Act & Assert
+            Assert.IsType<Task<DiffResult>>(diffApiService.GetDiff(id));
         }
     }
 }
